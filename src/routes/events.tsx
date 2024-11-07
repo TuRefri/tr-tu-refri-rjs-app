c1b303294b10cd723350d144074f4b13f17a66bc
LoOW3 <ignaciodiaznanni@gmail.com>
2024-11-07 15:39:07 -0300

    pt on events page

--- src/routes/events.tsx
+++ src/routes/events.tsx
@@ -3,7 +3,7 @@ import EventList from '../components/EventsList'
 
 export default function Events() {
   return (
-    <div className='w-full h-full pt-6 pb-10 px-4 flex flex-col items-center'>
+    <div className='w-full h-full pb-10 px-4 flex flex-col items-center pt-12'>
       <h1 className='w-full text-2xl font-semibold text-gray-700 px-1'>Eventos en tu barrio</h1>
       <section className='w-full h-full mt-2 pt-2'>
         <EventList />
