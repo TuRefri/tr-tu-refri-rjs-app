f4c6cf07776d6ee2d722286e5848e38cc969f9c4
LoOW3 <ignaciodiaznanni@gmail.com>
2024-11-19 11:07:30 -0300

    sidebar to user-profile

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -37,7 +37,7 @@ export default function SideBar() {
   return (
     <aside ref={sidebarRef} className=' pt-4 px-2 py-4 flex flex-col justify-center items-center h-[90%]'>
       <div className={`border border-gray-300 p-1 rounded-full`}>
-        <RoundedButton className={""} href='/profile' icon='icons/person.svg' />
+        <RoundedButton className={""} href='/user-profile' icon='icons/person.svg' />
       </div>
 
       <ul className={`${sideBarDim < 642? "mt-2" : "mt-6"} border border-gray-300 px-1 rounded-full flex flex-col justify-center items-center`}>
