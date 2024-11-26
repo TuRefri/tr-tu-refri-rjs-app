17a9aa97ab450a177d2c24e33d04886dae0be05c
LoOW3 <ignaciodiaznanni@gmail.com>
2024-11-26 08:07:22 -0300

    test pwa

--- vite.config.ts
+++ vite.config.ts
@@ -31,19 +31,34 @@ export default defineConfig({
         description:'pwa turefri',
         theme_color: '#19223c',
         background_color:'#d4d4d4',
-        icons:[
+        icons: [
           {
-            src:"pwa-512x512.png",
-            sizes:'512x512',
-            type:'image/png',
-            purpose: 'maskable'
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png"
           },
           {
-            src:"pwa-512x512.png",
-            sizes:'512x512',
-            type:'image/png',
-            purpose: 'any'
-          }
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "apple-touch-icon-180x180.png",
+            "sizes": "180x180",
+            "type": "image/png"
+          },
         ],
       }
     })]
