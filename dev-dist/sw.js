d49a1faf5267e530cdee99bbb936651873df99c5
LoOW3 <ignaciodiaznanni@gmail.com>
2024-12-04 14:38:06 -0300

    test federated identities

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.eem189urkho"
+    "revision": "0.n2aqjtdlbc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { Amplify } from 'aws-amplify';
+import config from './amplifyconfiguration.json'; // Ruta a tu archivo de configuración
 import { createRoot } from 'react-dom/client';
 import {
     createBrowserRouter,
@@ -5,10 +7,10 @@ import {
 } from "react-router-dom";
 import { Toaster } from 'sonner';
 import Root from "./routes/root";
-import TuRefri from './routes/turefri';
+/* import TuRefri from './routes/turefri'; */
 import ErrorPage from "./error-page";
 import './index.css';
-import {FridgeProvider} from './context/fridge-color-context.tsx'
+import { FridgeProvider } from './context/fridge-color-context.tsx';
 import Map from './routes/map.tsx';
 import Emergency from './routes/emergency.tsx';
 import Contact from './routes/contact.tsx';
@@ -16,75 +18,82 @@ import Scan from './routes/scan.tsx';
 import Events from './routes/events.tsx';
 import Profile from './routes/profile.tsx';
 import StorePage from './routes/stores.tsx';
-import Login from './routes/login.tsx'
-import { DetailMagnetProvider } from './context/detail-magnet-context.tsx';
+import Login from './routes/login.tsx';
+/* import { DetailMagnetProvider } from './context/detail-magnet-context.tsx'; */
 import { StorePageProvider } from './context/store-page-context.tsx';
 import { GlobalProvider } from './context/global-context.tsx';
 import SignUp from './routes/signup.tsx';
 import UserProfile from './routes/user-profile.tsx';
+import App from './components/App.tsx';
+
+{/* <DetailMagnetProvider>
+            <TuRefri />
+          </DetailMagnetProvider> */}
+
+// Configurar Amplify
+Amplify.configure(config);
+
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <GlobalProvider><Root /></GlobalProvider>,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: "/",
-          element: 
-          <DetailMagnetProvider>
-            <TuRefri />
-          </DetailMagnetProvider>,
-        },
-        {
-            path: "login",
-            element: <Login />, 
-        },
-        {
-            path: "signup",
-            element: <SignUp />, 
-        },
-        {
-            path: "profile",
-            element: <Profile />, 
-        },
-        {
-            path: "user-profile",
-            element: <UserProfile />, 
-        },
-        {
-            path: "stores",
-            element: 
-            <StorePageProvider>
-                <StorePage />
-            </StorePageProvider>, 
-        },
-        {
-            path: "stores/:id",
-            element: <StorePage />, 
-        },
-        {
-            path: "map",
-            element: <Map />, 
-        },
-        {
-            path: "events",
-            element: <Events/>, 
-        },
-        {
-            path: "emergency",
-            element: <Emergency />, 
-        },
-        {
-            path: "contact",
-            element: <Contact/>, 
-        },
-        {
-            path: "scan",
-            element: <Scan/>, 
-        },
-      ],
+        path: "/",
+        element: <GlobalProvider><Root /></GlobalProvider>,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <App></App>,
+            },
+            {
+                path: "login",
+                element: <Login />,
+            },
+            {
+                path: "signup",
+                element: <SignUp />,
+            },
+            {
+                path: "profile",
+                element: <Profile />,
+            },
+            {
+                path: "user-profile",
+                element: <UserProfile />,
+            },
+            {
+                path: "stores",
+                element: 
+                <StorePageProvider>
+                    <StorePage />
+                </StorePageProvider>, 
+            },
+            {
+                path: "stores/:id",
+                element: <StorePage />, 
+            },
+            {
+                path: "map",
+                element: <Map />, 
+            },
+            {
+                path: "events",
+                element: <Events/>, 
+            },
+            {
+                path: "emergency",
+                element: <Emergency />, 
+            },
+            {
+                path: "contact",
+                element: <Contact/>, 
+            },
+            {
+                path: "scan",
+                element: <Scan/>, 
+            },
+        ],
     },
-  ]);
+]);
+
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
@@ -97,6 +106,3 @@ if (rootElement) {
 } else {
     console.error("Element with ID 'root' not found");
 }
-
-
-
