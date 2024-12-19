2b3b14479d4d91d047458c6c2005543cb40fe8c9
LoOW3 <ignaciodiaznanni@gmail.com>
2024-12-19 17:39:57 -0300

    change map style

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2030c41c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.n65k9jecej"
+    "revision": "0.608meji27qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
