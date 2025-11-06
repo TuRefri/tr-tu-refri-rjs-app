a0b31880111c94b73c0fabbc4dd73041fecea058
Robin Ochoa <robin8a@gmail.com>
2025-11-06 11:18:04 -0500

    Project running locally

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,34 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "turefribackend": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "turefribackend8022d53a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "ADMINGroupRole": "string",
+      "MANAGERGroupRole": "string",
+      "MARKETINGADMINGroupRole": "string",
+      "REGULARGroupRole": "string"
+    }
+  },
+  "storage": {
+    "turefristorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
